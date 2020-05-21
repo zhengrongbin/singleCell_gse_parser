d73e258eb5db6a1d3084f3cc6c872114321554ae
Rongbin Zheng <zhengrongbin1992@163.com>
2020-05-21 21:20:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python scrna_parser_runner.py known -i need_gse.txt -gc 0 -p ./geo_gse/ -o geo_gse_scrna_collection_all_new2.txt
+### collection by a date range
+python scrna_parser_runner.py parser -d 2020/04/23-2020/05/21 -el ../geo_gse_collection.txt
+### collection by known GSE numbers
+#python scrna_parser_runner.py known -i need_gse.txt -gc 0 -p ./geo_gse/ -o geo_gse_scrna_collection_all_new2.txt
+### collection by a given direcotry where gse XML was saved
 #python scrna_parser_runner.py local -p ./geo_gse -o geo_gse_scrna_collection.txt
